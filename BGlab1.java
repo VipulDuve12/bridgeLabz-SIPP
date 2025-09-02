b466586d8b997e73bf228ca20b6e536a85446919
Vipul Duve <vduve123@gmail.com>
2025-09-02 09:02:42 +0530

    deleted all files

--- BGlab1.java
+++
@@ -1,5 +0,0 @@
-public class BGlab1{
-public static void main(String [] args){
-System.out.print("Welcome to Bridgelabz!");
-}
-}
--- BGlab3.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class BGlab3{
-public static void main(String [] args){
-Scanner sc = new Scanner(System.in);
-int celcius = sc.nextInt();
-int Fahrenheit = (celcius * 9/5) + 32;
-System.out.print(Fahrenheit);
-}
-}
